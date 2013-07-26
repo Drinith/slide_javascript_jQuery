c2230459aadff83db0b4b4452c487cbf009ac9ae
Felipe Fernandes <ripcorp@gmail.com>
2013-07-25 22:44:24 -0300

    Atualização e updates
    Dei uma otimizada em algumas partes do código e comentei algumas coisas.

--- js/script.js
+++ js/script.js
@@ -1,27 +1,29 @@
-
-			
-	var position=0;
+$(function() {						// Adiciona o document ready pra começar a brincadeira 
+													// só quando o HTML estiver pronto
+	var position = 0;
 	var min = 0;
-	var max= 1;
+	var max = 1;	
 	
-	
-		//Load the image 
-	loadImage= function (image) {
-	  $("#image").fadeOut('slow', function(){
-	    $("#image").load(function() {
-	      $(this).hide();
-	      $(this).fadeIn();
-	    }).attr('src',image);
-	  });
+	// Load the image 
+	var loadImage = function ( pImage )  				// declara a variavel função.									
+	{													// legal você usar um identificador que identifique o 			
+		var imageObject = $("#image");					// parametro de funcao.
+
+	  	imageObject.fadeOut('fast', function(){
+	    	imageObject.load(function() {
+	      		$(this).hide();
+	      		$(this).fadeIn();
+	    	}).attr('src', pImage);
+	  	});
 	}
 
-	
-	esquerda= function(){
-		
+	var esquerda = function() 
+	{
 		//andando com os valores para esquerda 
-		position-=1;				
-		//se posição ficar negativa refaça o loop para três fotos
-		if(position<min || position> max){
+		
+		position--;									// usa o modo reduzido de decremento
+		
+		if(position<min || position> max){			// se posição ficar negativa refaça o loop para três fotos
 			position=max;
 		}
 		
@@ -29,10 +31,11 @@
 		
 	}
 	
-	direita = function (){
-		
+	var direita = function () 
+	{
 		//andando com os valores para esquerda 
-		position+=1;
+
+		position++;									// usa o modo reduzido de incremento
 		
 		if(position>max || position<min){
 			position=min;
@@ -40,3 +43,14 @@
 		loadImage("img/image"+position+".jpg")
 		
 	}
+
+
+	//
+	// Comportamento para os botões sem precisar colocar no HTML
+
+	$('#setaesquerda').click(esquerda);
+	$('#setadireita').click(direita);
+
+	
+});
+		
