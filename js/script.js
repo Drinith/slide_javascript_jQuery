f338557a3b48d1f97520b436a2e80e75f4f4f69b
Felipe Mello <felpmel@gmail.com>
2013-07-29 15:13:33 -0300

    Modificações passadas pelo Felipe Fernandes

--- js/script.js
+++ js/script.js
@@ -1,42 +1,56 @@
-
 			
-	var position=0;
+	$(function() {						// Adiciona o document ready pra começar a brincadeira 
+													// só quando o HTML estiver pronto
+	var position = 0;
 	var min = 0;
-	var max= 1;
-	
-	
-		//Load the image 
-	loadImage= function (image) {
-	  $("#image").fadeOut('slow', function(){
-	    $("#image").load(function() {
-	      $(this).hide();
-	      $(this).fadeIn();
-	    }).attr('src',image);
-	  });
+	var max = 1;	
+
+	// Load the image 
+	var loadImage = function ( pImage )  				// declara a variavel função.									
+	{													// legal você usar um identificador que identifique o 			
+		var imageObject = $("#image");					// parametro de funcao.
+
+	  	imageObject.fadeOut('fast', function(){
+	    	imageObject.load(function() {
+	      		$(this).hide();
+	      		$(this).fadeIn();
+	    	}).attr('src', pImage);
+	  	});
 	}
 
-	
-	esquerda= function(){
-		
+	var esquerda = function() 
+	{
 		//andando com os valores para esquerda 
-		position-=1;				
-		//se posição ficar negativa refaça o loop para três fotos
-		if(position<min || position> max){
+
+		position--;									// usa o modo reduzido de decremento
+
+		if(position<min || position> max){			// se posição ficar negativa refaça o loop para três fotos
 			position=max;
 		}
-		
+
 		loadImage("img/image"+position+".jpg")
-		
+
 	}
-	
-	direita = function (){
-		
+
+	var direita = function () 
+	{
 		//andando com os valores para esquerda 
-		position+=1;
-		
+
+		position++;									// usa o modo reduzido de incremento
+
 		if(position>max || position<min){
 			position=min;
 		}
 		loadImage("img/image"+position+".jpg")
-		
+
 	}
+
+
+	//
+	// Comportamento para os botões sem precisar colocar no HTML
+
+	$('#setaesquerda').click(esquerda);
+	$('#setadireita').click(direita);
+
+
+});
